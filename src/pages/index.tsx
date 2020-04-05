770699c254007f2d27d9726e5b1630df2e9c0849
markokajzer <markokajzer91@gmail.com>
2020-04-05 12:10:11 +0200

    feat: mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,24 +4,17 @@ import Layout from '../components/Layout';
 import Profile from '../components/Profile';
 import Projects from '../components/Projects';
 
-const OPEN_WIDTH = 25;
-
 const IndexPage = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => setIsOpen(!isOpen);
 
   return (
     <Layout>
-      <div className="leftSide" style={{ width: `${isOpen ? OPEN_WIDTH : 100}%` }}>
+      <div className={`leftSide ${isOpen ? 'open' : 'closed'}`}>
         <Profile onClick={handleClick} />
       </div>
-      <div
-        className="rightSide"
-        style={{
-          width: `${100 - OPEN_WIDTH}vw`,
-          left: `${isOpen ? OPEN_WIDTH : 100}%`,
-        }}
-      >
+
+      <div className={`rightSide ${isOpen ? 'open' : 'closed'}`}>
         <Projects />
       </div>
     </Layout>
