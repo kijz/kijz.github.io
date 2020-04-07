a774371e483331203e9f000e134614ee729abf86
fkajzer <filipkajzer@gmail.com>
2020-04-07 21:05:57 +0200

    style carousel

--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -10,25 +10,39 @@ interface Slideshow {
 }
 
 class Slideshow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0,
+    };
+  }
+
+  // we check if we got event from input (and it has target property) or just value from Carousel
+  onChange = value => this.setState({ value });
   render() {
     return (
-      <Carousel arrows clickToChange>
-        <DefaultPlayer
-          muted
-          controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
-          poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
-        >
-          <source src="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm" type="video/webm" />
-        </DefaultPlayer>
-        <DefaultPlayer
-          muted
-          controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
-          poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
-        >
-          <source src="http://dl5.webmfiles.org/elephants-dream.webm" type="video/webm" />
-        </DefaultPlayer>
-        <Avatar />
-      </Carousel>
+      <>
+        <Carousel arrows value={this.state.value} onChange={this.onChange}>
+          <DefaultPlayer
+            key={1}
+            muted
+            controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
+            poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
+          >
+            <source src="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm" type="video/webm" />
+          </DefaultPlayer>
+          <Avatar key={2} />
+          <DefaultPlayer
+            key={3}
+            muted
+            controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
+            poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
+          >
+            <source src="http://dl5.webmfiles.org/elephants-dream.webm" type="video/webm" />
+          </DefaultPlayer>
+        </Carousel>
+        <Dots value={this.state.value} onChange={this.onChange} number={3} />
+      </>
     );
   }
 }
