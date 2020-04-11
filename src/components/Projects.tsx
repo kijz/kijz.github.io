62487fec5d1586ab436b828332401127c74c6062
fkajzer <filipkajzer@gmail.com>
2020-04-11 20:14:27 +0200

    proper video type, removed html5video dependency

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import Slideshow from './Slideshow';
+import Slideshow, { VideoSource } from './Slideshow';
 
 interface Project {
   name: string;
-  description: string;
-  URIs: string[];
+  description: string[];
+  videos: VideoSource[];
   technologies: string[];
 }
 
 const PROJECTS: Project[] = [
   {
-    name: 'Little Golden Retriever',
-    description:
-      'Most of us, as developers we spend plenty of time improving our programming skills: design patterns, meta-programming, good practices, new languages or frameworks, you name it. Furthermore, even deployment and infrastructure can be managed through code. Hence, we could say that most part of our days revolves around code, as a matter of fact when we aren’t writing code we are reading it',
-    URIs: ['https://assets.rbl.ms/510489/980x.jpg'],
-    technologies: ['Steam VR 2', 'Unity'],
+    name:
+      'Pre-operative Planning in Virtual Reality with Head Mounted Displays for Oral and Maxillofacial Surgery',
+    description: [
+      'Collaboration between the Visual Computing Department of the RWTH Aachen and the Oral and Maxillofacial derpartment of the University Hospital Aachen',
+      'Use only natural hand gestures like grabbing and pointing for usability',
+      'Most common procedures can be played out on visualized medical imaging of patients',
+      'Ability to save planned procedures and view / share them',
+      'In a collaboration with a fellow graduate student, planned procedures can be viewed while operating in Augmented Reality',
+      'Reduced time and strain on surgeons in the preparation phase for surgery',
+    ],
+    videos: [
+      {
+        poster: 'https://www.webmfiles.org/wp-content/uploads/2010/05/webm-file.jpg',
+        source: 'http://dl5.webmfiles.org/big-buck-bunny_trailer.webm',
+      },
+      {
+        poster: 'https://www.webmfiles.org/wp-content/uploads/2010/05/webm-files.jpg',
+        source: 'http://dl5.webmfiles.org/elephants-dream.webm',
+      },
+    ],
+    technologies: ['Steam VR 2', 'Unity', 'Valve Index'],
   },
   {
-    name: 'Little Golden Retriever 2',
-    description: 'Foo bar',
-    URIs: ['https://assets.rbl.ms/510489/980x.jpg'],
-    technologies: [],
+    name: 'Virtual Reality Lab (University project)',
+    description: [
+      'Recreate realistic or abstract interiors',
+      'Navigate and interact with environment by different means of ray casting',
+      'Choose from a set of textures, assets and colors to customize interior',
+      'Move into Sandbox environment and chose which asset to take with you and place freely in room',
+      'Showed that even a VR novice can build a room within ~20 min, experienced users ~5',
+    ],
+    videos: [
+      {
+        poster: 'https://www.webmfiles.org/wp-content/uploads/2010/05/webm-file.jpg',
+        source: 'http://dl5.webmfiles.org/big-buck-bunny_trailer.webm',
+      },
+      {
+        poster: 'https://www.webmfiles.org/wp-content/uploads/2010/05/webm-files.jpg',
+        source: 'http://dl5.webmfiles.org/elephants-dream.webm',
+      },
+    ],
+    technologies: ['Steam VR', 'Unity', 'HTC Vive'],
   },
 ];
 
@@ -30,11 +61,14 @@ const Projects: React.FC = () => {
       {PROJECTS.map(project => (
         <section key={project.name}>
           <h2>{project.name}</h2>
-          <p>{project.description}</p>
+          <ul>
+            {project.description.map((entry, index) => (
+              <li key={index}>{entry}</li>
+            ))}
+          </ul>
+          <Slideshow entries={project.videos} />
 
-          <Slideshow />
-
-          <p>Technologies: {project.technologies.join(' • ')}</p>
+          <p style={{ opacity: 0.5 }}>{project.technologies.join(' • ')}</p>
         </section>
       ))}
     </div>
--- src/components/Slideshow.tsx
+++ src/components/Slideshow.tsx
@@ -1,49 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
-import { DefaultPlayer } from 'react-html5video';
-import 'react-html5video/dist/styles.css';
-import Avatar from './Profile/Avatar';
 
-interface Slideshow {
-  URIs: string[];
+export interface VideoSource {
+  poster: string;
+  source: string;
 }
 
-class Slideshow extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0,
-    };
-  }
-
-  // we check if we got event from input (and it has target property) or just value from Carousel
-  onChange = value => this.setState({ value });
-  render() {
-    return (
-      <>
-        <Carousel arrows value={this.state.value} onChange={this.onChange}>
-          <DefaultPlayer
-            key={1}
-            muted
-            controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
-            poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
-          >
-            <source src="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm" type="video/webm" />
-          </DefaultPlayer>
-          <Avatar key={2} />
-          <DefaultPlayer
-            key={3}
-            muted
-            controls={['PlayPause', 'Seek', 'Time', 'Fullscreen']}
-            poster="http://dl5.webmfiles.org/big-buck-bunny_trailer.webm"
-          >
-            <source src="http://dl5.webmfiles.org/elephants-dream.webm" type="video/webm" />
-          </DefaultPlayer>
-        </Carousel>
-        <Dots value={this.state.value} onChange={this.onChange} number={3} />
-      </>
-    );
-  }
+interface Props {
+  entries: VideoSource[];
 }
+
+const Slideshow: React.FC<Props> = ({ entries }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const onChange = (value: number) => setCurrentSlide(value);
+
+  return (
+    <>
+      <Carousel arrows value={currentSlide} onChange={onChange}>
+        {entries.map((entry, index) => (
+          <video key={index} poster={entry.poster} controls muted>
+            <source src={entry.source} type="video/webm" /> {/* mp4? */}
+          </video>
+        ))}
+      </Carousel>
+      <Dots value={currentSlide} onChange={onChange} number={entries.length} />
+    </>
+  );
+};
 export default Slideshow;
