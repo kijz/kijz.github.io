c5f3b10d20b801bf8cbfe51a4b0a11cca909230e
markokajzer <markokajzer91@gmail.com>
2021-03-28 16:32:11 +0200

    chore: mobile fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,9 +46,13 @@ const IndexPage: React.FC<Props> = ({ data }) => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
+
+        {/* SEO */}
         <title>{data.site.siteMetadata.title}</title>
         <meta name="description" content={data.site.siteMetadata.description} />
         <meta name="author" content={data.site.siteMetadata.author} />
+
+        {/* Favicons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
