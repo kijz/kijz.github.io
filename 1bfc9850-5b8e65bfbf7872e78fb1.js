6cabe332ca402c5eecc97cd9ca9a52feb94373b8
Filip Kajzer <fkjzr@Filips-Air.fritz.box>
2022-08-28 19:25:52 +0200

    Updates

--- 1bfc9850-5b8e65bfbf7872e78fb1.js
+++ 1bfc9850-5b8e65bfbf7872e78fb1.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[2],{ma3e:function(t,n,a){"use strict";a.d(n,"a",(function(){return e}));var c=a("Lnxd");function e(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"}}]})(t)}}}]);
-//# sourceMappingURL=1bfc9850-a650189a03a832c67498.js.map
+//# sourceMappingURL=1bfc9850-5b8e65bfbf7872e78fb1.js.map
