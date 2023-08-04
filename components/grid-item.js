d452657185e422c5a64d18075d2f968e3c3df6bf
fkjzr <filipkajzer@gmail.com>
2023-08-04 13:01:42 +0200

    started a posts page, remove or add content later...

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,12 +1,13 @@
-import NextLink from 'next/link'
-import Image from 'next/image'
-import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
-import { Global } from '@emotion/react'
+import NextLink from "next/link";
+import Image from "next/image";
+import { Box, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
 
-export const GridItem = ({ children, href, title, thumbnail }) => {
+export const GridItem = ({ children, href, title, thumbnail }) => (
   <Box w="100%" align="center">
     <LinkBox cursor="pointer">
-      <Image src={thumbnail}
+      <Image
+        src={thumbnail}
         alt={title}
         className="grid-item-thumbnail"
         placeholder="blur"
@@ -19,13 +20,18 @@ export const GridItem = ({ children, href, title, thumbnail }) => {
       <Text fontSize={14}>{children}</Text>
     </LinkBox>
   </Box>
-}
+);
 
 export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
   <Box w="100%" align="center">
     <NextLink href={`/projects/${id}`}>
       <LinkBox cursor="pointer">
-        <Image src={thumbnail} alt={title} className="grid-item-thumbnail" placeholder="blur" />
+        <Image
+          src={thumbnail}
+          alt={title}
+          className="grid-item-thumbnail"
+          placeholder="blur"
+        />
         <LinkOverlay href={`/projects/${id}`}>
           <Text mt={2} fontSize={20}>{title}</Text>
         </LinkOverlay>
@@ -33,13 +39,14 @@ export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
       </LinkBox>
     </NextLink>
   </Box>
-)
+);
 
 export const GridItemStyle = () => (
-  <Global styles={`
+  <Global
+    styles={`
     .grid-item-thumbnail {
       border-radius:12px;
     }
   `}
   />
-)
+);
